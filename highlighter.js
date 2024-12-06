d325afc8bc35a80778802d2ce58eb4845cbbccf9
Gym Ikirenga <gymikirenga@Ikirengas-iMac.local>
2024-12-06 12:20:27 +0200

    new changes on the JS file

---
+++ highlighter.js
@@ -0,0 +1,18 @@
+// select the paragraph element 
+
+// Make all the text lower case remove all the punctuations 
+// count the words 
+// select words with high frequency 
+// reconstruct the paragraph after the selection of those with high frequency 
+// highlight the ones that are high in frequency and underline the one that starts with capital letter 
+
+document.addEventListener("DOMConteneLoader" , ()=> {
+    
+    // selecting the text paragraph into a variable and access it using textContent 
+
+    const paragraph = document.getElementById("myParagraph");
+    const text = paragraph.textContent;
+
+    // remmove punctuations and turn all text into lower ()
+
+})
